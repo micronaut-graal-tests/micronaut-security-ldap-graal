acfa28be164df35060b24d42ab599da1ee346183
Iván López <lopezi@objectcomputing.com>
2021-03-24 10:54:03 +0100

    Add `@Secured` Controller

--- src/main/java/example/micronaut/Foo.java
+++
@@ -1,7 +0,0 @@
-package example.micronaut;
-
-import io.micronaut.core.annotation.Introspected;
-
-@Introspected
-public class Foo {
-}
---
+++ src/main/java/example/micronaut/HomeController.java
@@ -0,0 +1,19 @@
+package example.micronaut;
+
+import io.micronaut.http.MediaType;
+import io.micronaut.http.annotation.Controller;
+import io.micronaut.http.annotation.Get;
+import io.micronaut.security.annotation.Secured;
+import io.micronaut.security.rules.SecurityRule;
+
+import java.security.Principal;
+
+@Controller
+@Secured(SecurityRule.IS_AUTHENTICATED)
+public class HomeController {
+
+    @Get(produces = MediaType.TEXT_PLAIN)
+    String index(Principal principal) {
+        return principal.getName();
+    }
+}
